a10cade78f5f0a7441a4c4c6f3133d00f3f5b6b0
Yunus <37776208+Karazakoff@users.noreply.github.com>
2019-12-20 16:30:29 +0200

    Update problem_1.cs

--- session_2/problem_1.cs
+++ session_2/problem_1.cs
@@ -1,9 +1,23 @@
 using System;
 
 namespace std{
-  class Solution(){
+  class Solution{
       static void Main(string[] args){
-        
+        int number = Convert.ToInt32(Console.ReadLine());
+        int[,] tab = new int[number, number];
+        for(int i = 0 ; i < number; i ++){
+          tab[i,0] = 1;
+          tab[0,i] = 1;
+          tab[number - 1, i] = 1;
+          tab[i, number - 1] = 1;
+        }
+        Console.WriteLine();
+        for(int i = 0 ; i < number ; i ++){
+          for(int j = 0 ; j < number ; j ++){
+            Console.Write(tab[i,j] + " ");
+          }
+          Console.WriteLine();
+        }
       }
   }
 }
