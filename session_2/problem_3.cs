0f1c34b0ed226f12f1fc18d88b0060f48884b618
Yunus <37776208+Karazakoff@users.noreply.github.com>
2019-12-20 16:50:49 +0200

    Update problem_3.cs

--- session_2/problem_3.cs
+++ session_2/problem_3.cs
@@ -1,9 +1,35 @@
 using System;
 
 namespace std{
-  class Solution(){
+  class Solution{
       static void Main(string[] args){
+        int number;
+        do{
+          number = Convert.ToInt32(Console.ReadLine());
+          int[,] tab = new int[number, number];
+          int counter = 1;
+          for(int i = 0 ; i < number ; i ++){
+            if(counter == 4){
+              counter = 1;
+            }
+            for(int j = 0 ; j < number ; j ++){
+              if(counter == 4){
+                counter = 1;
+              }
+              tab[i,j] = counter;
+              counter ++;
+            }
+          }
+          for(int i = 0 ; i < number ; i ++){
+            for(int j = 0 ; j < number ; j ++){
+              Console.Write(tab[i,j] + " ");
+            }
+            Console.WriteLine();
+          }
 
+        }while(number != 0);
+
+        Console.WriteLine("Finished. run your code again!");
       }
   }
 }
